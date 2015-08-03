f0381dd84d57aabe5bdeebb5dc52590320deac4a
ernestyj@outlook.com <ernestyj@outlook.com>
2015-08-03 10:39:54 +0800

    add resttest

--- resttest/resttest.js
+++ resttest/resttest.js
@@ -3,7 +3,6 @@ var expect = require('expect.js')
 
 describe('express rest api server', function(){
 	var id
-	
 	it('posts an object', function(done){
 		superagent.post('http://localhost:3000/collections/test')
 			.send({name:'ernest', email:'ernestyj@outlook.com'})
@@ -16,4 +15,58 @@ describe('express rest api server', function(){
 				done()
 			})
 	})
+	it('retrieves an object', function(done){
+		superagent.get('http://localhost:3000/collections/test/' + id)
+			.end(function(e, res){
+				console.log(res.body)
+				expect(e).to.eql(null)
+				expect(typeof res.body).to.eql('object')
+				expect(res.body._id.length).to.eql(24)
+				expect(res.body._id).to.eql(id)
+				done()
+			})
+	})
+	it('retrieves a collection', function(done){
+		superagent.get('http://localhost:3000/collections/test')
+			.end(function(e, res){
+				console.log(res.body)
+				expect(e).to.eql(null)
+				expect(res.body.length).to.be.above(_id)
+				expect(res.body.map(function(item){return item._id})).to.contain(id)
+				done()
+			})
+	})
+	it('updates an object', function(done){
+		superagent.put('http://localhost:3000/collections/test/' + id)
+			.send({name: 'Peter', email:'peter@yahoo.com'})
+			.end(function(e, res){
+				console.log(res.body)
+				expect(e).to.eql(null)
+				expect(typeof res.body).to.eql('object')
+				expect(res.body.msg).to.eql('success')
+				done()
+			})
+	})
+	it('checks an updated object', function(done){
+    superagent.get('http://localhost:3000/collections/test/'+id)
+      .end(function(e, res){
+        // console.log(res.body)
+        expect(e).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body._id.length).to.eql(24)
+        expect(res.body._id).to.eql(id)
+        expect(res.body.name).to.eql('Peter')
+        done()
+      })
+  })
+  it('removes an object', function(done){
+    superagent.del('http://localhost:3000/collections/test/'+id)
+      .end(function(e, res){
+        // console.log(res.body)
+        expect(e).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body.msg).to.eql('success')
+        done()
+      })
+  })
 })
